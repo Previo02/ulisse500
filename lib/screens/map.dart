fb9b38e436d356e358cab66022167e1ba0c7a01d
Previo02 <alessandro.previati@studio.unibo.it>
2024-08-01 15:50:09 +0200

    Added map with marker

--- lib/screens/map.dart
+++ lib/screens/map.dart
@@ -1,7 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
+import 'package:url_launcher/url_launcher_string.dart';
 
-class MapPage extends StatelessWidget {
+class MapPage extends StatefulWidget {
   const MapPage({super.key});
+
+  @override
+  State<MapPage> createState() => _MapPageState();
+}
+
+class _MapPageState extends State<MapPage> {
+  final List<Marker> _markers = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _initializeMarkers();
+  }
+
+  void _initializeMarkers() {
+    _markers.add(
+      Marker(
+        width: 80.0,
+        height: 80.0,
+        point: const LatLng(44.494887, 11.342616),
+        child: IconButton(
+          icon: const Icon(Icons.location_on),
+          color: Colors.red,
+          iconSize: 40.0,
+          onPressed: () {
+            _openGooglePage(
+                'https://www.google.com/search?q=Museo+di+Palazzo+Poggi');
+          },
+        ),
+      ),
+    );
+  }
+
+  void _openGooglePage(String url) async {
+    if (await canLaunchUrlString(url)) {
+      await launchUrlString(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,8 +53,20 @@ class MapPage extends StatelessWidget {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: const Text("Ulisse500"),
       ),
-      body: const Center(
-        child: Text("Map Page"),
+      body: FlutterMap(
+        options: const MapOptions(
+          initialCenter: LatLng(44.494887, 11.342616),
+          minZoom: 0,
+          maxZoom: 150.0,
+        ),
+        children: [
+          TileLayer(
+            urlTemplate: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+          ),
+          MarkerLayer(
+            markers: _markers,
+          ),
+        ],
       ),
     );
   }
