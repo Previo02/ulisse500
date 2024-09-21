a563842e569a359e4ee0b474fb8c8808fa911c67
Previo02 <alessandro.previati@studio.unibo.it>
2024-09-21 16:40:26 +0200

    shark.glb added, doesnt work, too much memory usage, work fine with felis

--- lib/screens/ar/ar_view_ios.dart
+++ lib/screens/ar/ar_view_ios.dart
@@ -1,4 +1,3 @@
-import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'package:arkit_plugin/arkit_plugin.dart';
 import 'package:vector_math/vector_math_64.dart' as vector;
@@ -37,24 +36,9 @@ class ARViewIOSState extends State<ARViewIOS> {
         children: [
           ARKitSceneView(
             enableTapRecognizer: true,
-            onARKitViewCreated: (ARKitController controller) {
-              this.controller = controller;
-              controller.onARTap = (hits) {
-                final point = hits.firstWhereOrNull(
-                  (hit) => hit.type == ARKitHitTestResultType.featurePoint,
-                );
-                if (point != null) {
-                  try {
-                    log('Hit detected at: ${point.worldTransform.getColumn(3)}');
-                    _onARTapHandler(point);
-                  } catch (e) {
-                    log('Error during AR tap handling: $e');
-                  }
-                } else {
-                  log('No valid feature point detected.');
-                }
-              };
-            },
+            showFeaturePoints: true,
+            planeDetection: ARPlaneDetection.horizontalAndVertical,
+            onARKitViewCreated: onARKitViewCreated,
           ),
           Align(
             alignment: Alignment.topCenter,
@@ -80,7 +64,19 @@ class ARViewIOSState extends State<ARViewIOS> {
     );
   }
 
-  Future<void> _onARTapHandler(ARKitTestResult point) async {
+  void onARKitViewCreated(ARKitController arkitController) {
+    controller = arkitController;
+    controller.onARTap = (ar) {
+      final point = ar.firstWhereOrNull(
+        (o) => o.type == ARKitHitTestResultType.featurePoint,
+      );
+      if (point != null) {
+        _onARTapHandler(point);
+      }
+    };
+  }
+
+  void _onARTapHandler(ARKitTestResult point) {
     final position = vector.Vector3(
       point.worldTransform.getColumn(3).x,
       point.worldTransform.getColumn(3).y,
@@ -89,30 +85,20 @@ class ARViewIOSState extends State<ARViewIOS> {
 
     if (currentNode != null) {
       currentNode!.position = position;
+      controller.update(currentNode!.name);
     } else {
-      try {
-        final node = ARKitGltfNode(
-          assetType: AssetType.flutterAsset,
-          url: "assets/models/felis.glb",
-          scale: vector.Vector3.all(0.5),
-          position: position,
-        );
-        await controller.add(node);
-        currentNode = node;
-      } catch (e) {
-        log('Error loading model: $e');
-      }
+      currentNode = _getNodeFromFlutterAsset(position);
+      controller.add(currentNode!);
     }
   }
 
-  /*ARKitGltfNode _getNodeFromFlutterAsset(vector.Vector3 position) {
-    return ARKitGltfNode(
-      assetType: AssetType.flutterAsset,
-      url: "assets/models/felis.glb",
-      scale: vector.Vector3.all(0.5),
-      position: position,
-    );
-  }*/
+  ARKitGltfNode _getNodeFromFlutterAsset(vector.Vector3 position) =>
+      ARKitGltfNode(
+        assetType: AssetType.flutterAsset,
+        url: 'assets/models/shark.glb',
+        scale: vector.Vector3(0.01, 0.01, 0.01),
+        position: position,
+      );
 
   @override
   void dispose() {
